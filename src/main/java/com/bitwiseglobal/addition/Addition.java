85a5ba77a4f5d6de99ea2d34958af1dfe49c6ecb
balajeev <Balajee.Venkatesh@bitwiseglobal.com>
2016-02-12 12:37:48 +0530

    Initial commit

---
+++ src/main/java/com/bitwiseglobal/addition/Addition.java
@@ -0,0 +1,21 @@
+package com.bitwiseglobal.addition;
+
+/**
+ * Created by balajeev on 2/5/2016.
+ */
+public class Addition {
+    public int add(int no1, int no2) {
+
+        return (no1+no2);
+    }
+
+    public int sub(int no1, int no2) {
+
+        return (no1-no2);
+    }
+
+    public static void main(String args[])
+    {
+        System.out.println("Build");
+    }
+}
---
+++ src/test/java/com/bitwiseglobal/addition/AdditionTest.java
@@ -0,0 +1,32 @@
+package com.bitwiseglobal.addition;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by balajeev on 2/5/2016.
+ */
+public class AdditionTest {
+
+ @Test
+
+    public void itShouldAddTwoNumbers()
+ {
+     Addition ob=new Addition();
+
+     assertEquals(10,ob.add(10,20));
+ }
+
+    @Test
+
+    public void itShouldSubtractTwoNumbers()
+    {
+        Addition ob=new Addition();
+
+        assertEquals(10,ob.sub(20,10));
+    }
+
+
+
+}
